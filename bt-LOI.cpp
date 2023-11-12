2cc4c1af8fe750c050dc515f88114e159552c308
TranDinhVo <138351394+TranDinhVo@users.noreply.github.com>
2023-11-12 13:15:30 +0700

    Update bt1-LOI.cpp

--- bt-LOI.cpp
+++ bt-LOI.cpp
@@ -13,11 +13,10 @@ int main(void){
     int ss = 0;
     int dem = 0;
     for (int j = 1; j <= n; j++){
-        for (int i = 1; i <= 2 ; i++ ){
+        for (int i = 1; i <= 1 ; i++ ){
             
             if ( a[i][j] >= a[i+1][j] )
                 dem += 1;
-            
             else
             {
                 if ( dem > ss ){
@@ -25,11 +24,10 @@ int main(void){
                     dem = 0;
                 }
                 else dem = 0;
+                
             }
-            
         }
     }
-    
     printf ("KQ : %d ", ss);
     return 0;
 }
