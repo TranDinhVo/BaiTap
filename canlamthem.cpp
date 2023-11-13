91ba44ae80703c32e16203495c91eb5b164ee321
TranDinhVo <138351394+TranDinhVo@users.noreply.github.com>
2023-11-13 13:46:42 +0700

    Update canlamthem.cpp

--- canlamthem.cpp
+++ canlamthem.cpp
@@ -4,31 +4,24 @@
 //
 //int main(void){
 //    int a, b, c;
-//    
+//
 //    printf ("Nhap a = ");
 //    scanf ("%d", &a);
 //    printf ("Nhap b = ");
 //    scanf ("%d", &b);
 //    printf ("Nhap c = ");
 //    scanf ("%d", &c);
-//     
-//    if ( a > b )
-//    {
-//        if (a > c)
-//            printf ("So lon nhat la : %d", a);
-//        else
-//            printf ("So lon nhat la : %d", c);
-//    }
-//    else{
-//        if (b > c)
-//            printf ("So lon nhat la : %d", b);
-//        else
-//            printf ("So lon nhat la : %d", c);
-//    }
-//    
+//
+//    int max = a;
+//    if ( max < b )
+//        max = b;
+//    if ( max < c)
+//        max = c;
+//    printf ("so lon nhat la : %d ", max);
 //    printf ("\n");
 //    return 0;
 //}
+
 
 // Nhập vào tháng của 1 năm. Cho biết tháng thuộc quý mấy trong năm.
 //#include <stdio.h>
